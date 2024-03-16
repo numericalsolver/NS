74442cf9dc4d5d53c3e8192430357cb8a13e5873
Tanvi0209 <dappilitanvi@gmail.com>
2024-03-17 00:33:12 +0530

    Correct code

--- tftoss.py
+++ tftoss.py
@@ -1,112 +1,29 @@
-# import numpy as np
-
-# # Get input for the numerator coefficients of the transfer function
-# numerator_str = input("Enter the numerator coefficients of the transfer function separated by spaces: ")
-# numerator = list(map(float, numerator_str.split()))
-
-# # Get input for the denominator coefficients of the transfer function
-# denominator_str = input("Enter the denominator coefficients of the transfer function separated by spaces: ")
-# denominator = list(map(float, denominator_str.split()))
-
-# # Ensure the denominator is not empty
-# if not denominator:
-#     print("Denominator coefficients cannot be empty. Exiting.")
-#     exit()
-
-# # Extract the order of the transfer function
-# order = len(denominator) - 1
-
-# # Create the state space model matrices
-# A = np.zeros((order, order))
-# B = np.zeros((order, 1))
-# C = np.zeros((1, order))
-# D = np.zeros((1, 1))
-
-# # Populate the state space model matrices
-# A[:-1, 1:] = np.eye(order - 1)
-# A[-1, :] = -np.array(denominator[:-1])
-
-# B[-1, 0] = 1
-# #C[0, :len(numerator)] = numerator
-# C[0, :] = np.flip(numerator[:-1])
-# D[0, 0] = numerator[-1]
-# #D[0, 0] = 0  # D matrix should be zeros for transfer functions
-
-# # Display the state space model matrices
-# print("State Space Model Matrices:")
-# print("A:\n", A)
-# print("B:\n", B)
-# print("C:\n", C)
-# print("D:\n", D)
-
-
-# import numpy as np
-
-# # Transfer function coefficients
-# numerator = [1, 3, 3]
-# denominator = [1, 2, 3, 1]
-
-# # Extract the order of the transfer function
-# order = len(denominator) - 1
-
-# # Create the state space model matrices
-# A = np.zeros((order, order))
-# B = np.zeros((order, 1))
-# C = np.zeros((1, order))
-# D = np.zeros((1, 1))
-
-# # Populate the state space model matrices
-# A[:-1, 1:] = np.eye(order - 1)
-# A[-1, :] = -np.array(denominator[1:])
-# B[-1, 0] = 1
-
-# # Check if the numerator has enough coefficients
-# if len(numerator) < order:
-#     print("Insufficient numerator coefficients provided. Exiting.")
-#     exit()
-
-# # Assign coefficients from numerator to C in reverse order
-# C[0, :] = np.flip(numerator[:-1])
-# D[0, 0] = numerator[-1]
-
-# # Display the state space model matrices
-# print("State Space Model Matrices:")
-# print("A:\n", A)
-# print("B:\n", B)
-# print("C:\n", C)
-# print("D:\n", D)
-
-
-
-
 import numpy as np
 
-# Transfer function coefficients
-numerator = [1, 3, 3]
-denominator = [1, 2, 3, 1]
+# Take input from the user for numerator coefficients
+numerator_str = input("Enter the numerator coefficients separated by spaces: ")
+numerator = list(map(float, numerator_str.split()))
 
-# Extract the order of the transfer function
-order = len(denominator) - 1
+# Take input from the user for denominator coefficients
+denominator_str = input("Enter the denominator coefficients separated by spaces: ")
+denominator = list(map(float, denominator_str.split()))
+
+# Convert transfer function to state space model
+numerator_pad = numerator + [0] * (len(denominator) - len(numerator))
+denominator_pad = denominator + [0] * (len(numerator) - len(denominator))
 
 # Create the state space model matrices
-A = np.zeros((order, order))
-B = np.zeros((order, 1))
-C = np.zeros((1, order))
-D = np.zeros((1, 1))
-
-# Populate the state space model matrices
-A[:-1, 1:] = np.eye(order - 1)
-A[-1, :] = -np.array(denominator[1:])
+A = np.zeros((len(denominator_pad) - 1, len(denominator_pad) - 1))
+A[:-1, 1:] = np.eye(len(denominator_pad) - 2)
+A[-1, :] = -np.array(denominator_pad[1:])
+B = np.zeros((len(denominator_pad) - 1, 1))
 B[-1, 0] = 1
+matrix= np.array([[numerator_pad[0], numerator_pad[1], numerator_pad[2]]])
+C = matrix[:,::-1]
+D = np.array([[0]])
 
-# Check if the numerator has enough coefficients
-if len(numerator) < order:
-    print("Insufficient numerator coefficients provided. Exiting.")
-    exit()
-
-# Assign coefficients from reversed numerator to C
-C[0, -len(numerator[:-1]):] = np.flip(numerator[:-1])
-D[0, 0] = numerator[-1]
+# Reverse the last row of matrix A
+A[-1, :] = A[-1, ::-1]
 
 # Display the state space model matrices
 print("State Space Model Matrices:")
